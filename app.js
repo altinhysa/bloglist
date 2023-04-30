ecbbb65c0c3c0e035218fcd22bc637efbe56986c
Altin Hysa <altinhysa84@gmail.com>
2023-05-01 00:23:39 +0200

    add files

---
+++ app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+module.exports = app
